929659ddfe14f1e5c21667c0d0c9d8a521b3747d
toddmac12 <toddmac12@gmail.com>
2021-10-29 14:23:10 -0400

    Edits to Park.cs file.

--- NationalParksApi/Models/NationalParksApiContext.cs
+++ NationalParksApi/Models/NationalParksApiContext.cs
@@ -3,21 +3,21 @@ using System;
 
 namespace NationalParksApi.Models
 {
-    public class NationalParksApiContext : DbContext
+  public class NationalParksApiContext : DbContext
+  {
+    public NationalParksApiContext(DbContextOptions<NationalParksApiContext> options)
+        : base(options)
     {
-        public NationalParksApiContext(DbContextOptions<NationalParksApiContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Park> Parks { get; set; }
-
- protected override void OnModelCreating(ModelBuilder builder)
-      {
-        builder.Entity<Park>()
-        .HasData (new Park { ParkId =1, Name = "Park Name", Location ="Park Location", AreaKmSquared = 500, Description = "Park Description"});
-
-     
-      }
     }
+
+    public DbSet<Park> Parks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+      .HasData(new Park { ParkId = 1, Name = "Park Name", Location = "Park Location", AreaKmSquared = 500, Description = "Park Description" });
+
+
+    }
+  }
 }
--- NationalParksApi/Models/Park.cs
+++ NationalParksApi/Models/Park.cs
@@ -4,10 +4,15 @@ namespace NationalParksApi.Models
 {
   public class Park
   {
-    public int ParkId {get; set;}
-public string Name {get; set;}
-  public string Location {get; set;}
-  public float AreaKmSquared {get; set;}
-  public string Description {get; set;}
+    public int ParkId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Location { get; set; }
+    [Required]
+    [Range(1, 50000, ErrorMessage = "Must be between 1 and 50000")]
+    public float AreaKmSquared { get; set; }
+    [Required]
+    public string Description { get; set; }
   }
 }
